df68e5a0fb7537fb4e1fab9d576d03e147614cf9
AslamAsh <93033181+AslamAsh@users.noreply.github.com>
2026-01-23 00:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var isDate = function (input) {
   //   write your code here
-	 return /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/.test(date);
+	  return /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/.test(input);
 };
 
 // Do not change the code below.
