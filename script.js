8bb485b04ae8ac099c22580407a3564bb05666d3
AslamAsh <93033181+AslamAsh@users.noreply.github.com>
2026-01-23 00:32:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 var isDate = function (input) {
   //   write your code here
-	  if (input === null || input === undefined) {
+	   if (typeof input !== "string") {
     return false;
   }
 
-  // Convert anything to string safely
-  input = String(input).trim();
+  input = input.trim();
 
   return /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/.test(input);
 };
